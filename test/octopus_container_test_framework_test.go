2fe3d6fb5272688c9d4e28f9bc20630a07bc15ad
Matthew Casperson <matthewcasperson@gmail.com>
2024-07-20 07:15:47 +1000

    Merged changes from main

--- test/octopus_container_test_framework_test.go
+++ test/octopus_container_test_framework_test.go
@@ -1,6 +1,10 @@
 package test
 
 import (
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/environments"
+	"github.com/OctopusSolutionsEngineering/OctopusTerraformTestFramework/octoclient"
+	"path/filepath"
 	"testing"
 )
 
@@ -59,4 +63,38 @@ func TestContainerWithNoSpecifiedVersionWillUseLatest(t *testing.T) {
 	if version != "latest" {
 		t.Errorf("The OctopusServer version is %v", version)
 	}
+}
+
+// TestCreateEnvironments is an example of the kind of tests that can be written using the OctopusContainerTest framework.
+func TestCreateEnvironments(t *testing.T) {
+	testFramework := OctopusContainerTest{}
+	testFramework.ArrangeTest(t, func(t *testing.T, container *OctopusContainer, client *client.Client) error {
+		// Act
+		newSpaceId, err := testFramework.Act(
+			t,
+			container,
+			filepath.Join("..", "terraform"), "2-simpleexample", []string{})
+
+		if err != nil {
+			return err
+		}
+
+		newSpaceClient, err := octoclient.CreateClient(container.URI, newSpaceId, ApiKey)
+
+		if err != nil {
+			return err
+		}
+
+		testEnvironments, err := environments.GetAll(newSpaceClient, newSpaceId)
+
+		if err != nil {
+			return err
+		}
+
+		if len(testEnvironments) != 3 {
+			t.Fatalf("Expected 3 environments, got %d", len(testEnvironments))
+		}
+
+		return nil
+	})
 }
