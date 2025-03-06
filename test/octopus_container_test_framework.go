66fe08ad29645fb3f605ddf9f8f31c2f74ab7cc0
domenicsim1 <domenic.simone@octopus.com>
2025-03-07 09:52:02 +1100

    nil entry point

--- test/octopus_container_test_framework.go
+++ test/octopus_container_test_framework.go
@@ -218,8 +218,9 @@ func (o *OctopusContainerTest) setupOctopus(ctx context.Context, connString stri
 		Networks: []string{
 			network,
 		},
-		Entrypoint: []string{"/bin/sh"},
+		Entrypoint: nil,
 		Cmd: []string{
+			"/bin/sh",
 			"-c",
 			"wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg && " +
 				"echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | tee /etc/apt/sources.list.d/hashicorp.list && " +
