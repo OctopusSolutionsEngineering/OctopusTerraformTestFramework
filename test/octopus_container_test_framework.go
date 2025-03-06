2de83d62541f5891222c16a05234f2607c4c28cd
domenicsim1 <domenic.simone@octopus.com>
2025-03-06 11:37:47 +1100

    fix Cmd commands

--- test/octopus_container_test_framework.go
+++ test/octopus_container_test_framework.go
@@ -219,8 +219,10 @@ func (o *OctopusContainerTest) setupOctopus(ctx context.Context, connString stri
 			network,
 		},
 		Cmd: []string{
-			"wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg",
-			"echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | tee /etc/apt/sources.list.d/hashicorp.list",
+			"/bin/bash",
+			"-c",
+			"wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg;",
+			"echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | tee /etc/apt/sources.list.d/hashicorp.list;",
 			"apt update -y && apt install terraform -y",
 		},
 	}
