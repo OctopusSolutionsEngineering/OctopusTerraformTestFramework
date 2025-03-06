d9f16912c9cdd5279e9f8181081af85fb41d868b
domenicsim1 <domenic.simone@octopus.com>
2025-03-07 10:56:05 +1100

    drop cmd scripts

--- test/octopus_container_test_framework.go
+++ test/octopus_container_test_framework.go
@@ -218,15 +218,6 @@ func (o *OctopusContainerTest) setupOctopus(ctx context.Context, connString stri
 		Networks: []string{
 			network,
 		},
-		Entrypoint: nil,
-		Cmd: []string{
-			"/bin/sh",
-			"-c",
-			"wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg && " +
-				"echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | tee /etc/apt/sources.list.d/hashicorp.list && " +
-				"apt update && apt install -y terraform && " +
-				"./install.sh",
-		},
 	}
 
 	req.Env = o.AddCustomEnvironment(req.Env)
