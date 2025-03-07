0d83fd2b1e492453bb002ab60e6ec01a1f48f690
domenicsim1 <87625140+domenicsim1@users.noreply.github.com>
2025-03-07 11:16:52 +1100

    Fix/testing

--- test/octopus_container_test_framework.go
+++ test/octopus_container_test_framework.go
@@ -218,15 +218,6 @@ func (o *OctopusContainerTest) setupOctopus(ctx context.Context, connString stri
 		Networks: []string{
 			network,
 		},
-		Entrypoint: []string{"/bin/bash"},
-		Cmd: []string{
-			"-c",
-			"apt-get update && apt-get install -y curl gnupg lsb-release && " +
-				"curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg && " +
-				"echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | tee /etc/apt/sources.list.d/hashicorp.list > /dev/null && " +
-				"apt-get update && apt-get install -y terraform && " +
-				"./install.sh",
-		},
 	}
 
 	req.Env = o.AddCustomEnvironment(req.Env)
